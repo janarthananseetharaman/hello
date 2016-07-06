8150c92fd209e55ea395c05b88f170b4282d6270
janarthananseetharaman <janarthananksrce@gmail.com>
2016-07-06 14:56:30 +0530

    Create porn.java

---
+++ porn.java
@@ -0,0 +1,10 @@
+public class jana {
+	public static void main(String[] args) {
+		int b=-5;
+		if(b>0)
+           System.out.println("The given number is positive");
+           else
+        	   System.out.println("The given number is negative");
+     }
+
+}
