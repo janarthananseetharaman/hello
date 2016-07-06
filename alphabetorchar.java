f502a6f441f22db3887bed050928428dcd27e098
janarthananseetharaman <janarthananksrce@gmail.com>
2016-07-06 14:51:23 +0530

    Create alphabetorchar.java

---
+++ alphabetorchar.java
@@ -0,0 +1,10 @@
+public class jana {
+	public static void main(String[] args) {
+		char b='a';
+		if(b>='a'&&b<='z')
+           System.out.println("The given charecter is alphabet");
+           else
+        	   System.out.println("The given charecter is number");
+     }
+
+}
