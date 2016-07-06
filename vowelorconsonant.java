98eac2c546935ac49637132b69773ec16f887ba1
janarthananseetharaman <janarthananksrce@gmail.com>
2016-07-06 14:46:15 +0530

    Create vowelorconsonant.java

---
+++ vowelorconsonant.java
@@ -0,0 +1,10 @@
+public class jana {
+	public static void main(String[] args) {
+		char b='e';
+		if(b=='a'||b=='e'||b=='i'||b=='o'||b=='u')
+           System.out.println("The given char is vowel");
+           else
+        	   System.out.println("The given char is consonant");
+     }
+
+}
