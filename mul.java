974265eb1f1c20e600a4663828cf27253afa03a3
janarthananseetharaman <janarthananksrce@gmail.com>
2016-07-16 05:12:20 -0700

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,13 @@
+
+public class mul {
+
+	
+	public static void main(String[] args) {
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+int c=a*b;
+String d=String.valueOf(c);
+System.out.println("The multiplication of two string integer is:"+d);
+	}
+
+}
