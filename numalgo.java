c7515b571d7dcd2fa48b96fa09907909df3f397e
janarthananseetharaman <janarthananksrce@gmail.com>
2016-07-16 00:06:45 -0700

    Create numalgo.java

---
+++ numalgo.java
@@ -0,0 +1,4 @@
+  To store the 1st element in a variable.
+  Then by using for loop check the number is present in another place or not
+  If it present then print "The 1st number of array is repeated"
+  else print "The number is not repeated
